0997e84b3aace4563ac7d01957ac7667ec93f469
ThiagoDuart <td1405150+github@gmail.com>
2023-06-14 08:41:30 -0300

    entrega

--- src/scripts/range.js
+++ src/scripts/range.js
@@ -3,12 +3,12 @@ import {filterAlbumPrice} from "./index.js";
 
 export function inputRange(product) {
     const price = document.getElementById('price');
-    const temp = document.getElementById('temp');
+    const valueRealTime = document.getElementById('temp');
 
 
 price.addEventListener('input', () => {
   let priceValue = price.value;
-  temp.textContent = parseFloat(priceValue).toFixed(2)
+  valueRealTime.textContent = parseFloat(priceValue).toFixed(2)
   filterAlbumPrice(product,priceValue)
     
 });
